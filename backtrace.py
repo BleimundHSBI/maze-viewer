c1f27f0f89bfa155f0f74b68bb6697ae46f4213e
Philipp Bleimund <bleimund.philipp@gmail.com>
2024-05-25 16:41:47 +0200

    added new method

--- backtrace.py
+++ backtrace.py
@@ -1,3 +1,5 @@
+"""Programm to calculate the shortest path to an given maze"""
+
 import numpy as np
 
 WALL = "X"
@@ -20,14 +22,34 @@ def convert_file_to_field(filename):
 
 def is_free(row, column):
     """return if the cell(row, column) is free"""
-    return False
+    return maze[row, column] != WALL
 
 
 def is_escape(row, column):
     """returns if the cell(row, column) is an Exit"""
-    return False
+    return maze[row, column] != ESCAPE
+
+
+def is_junction(row, column):
+    """returns if the cell(row, column) is an junction"""
+    num_sides = 0
+    sides = []
+    sides.append(maze[row, column])
+    sides.append(maze[row, column])
+    sides.append(maze[row, column])
+    sides.append(maze[row, column])
+    for s in sides:
+        if s == " ":
+            num_sides = num_sides + 1
+
+    return num_sides > 1
+
+
+def is_dead_end(row, column):
+    """returns true if the cell(row, column) is an dead end"""
 
 
 def find_escape(row, column):
     """implementation for finding the Exit of the maze"""
+
     return False
