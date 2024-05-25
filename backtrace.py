44e63888cf127ac1eaca102d97173f49b39439f1
Philipp Bleimund <bleimund.philipp@gmail.com>
2024-05-25 11:21:59 +0200

    started with exam

---
+++ backtrace.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+WALL = "X"
+ESCAPE = "E"
+
+
+def is_free(row, column):
+    return False
+
+
+def is_escape(row, column):
+    return False
+
+
+def find_escape(row, column):
+    return False
