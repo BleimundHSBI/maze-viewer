82d4a618d333444445b22f833f5ae86e9966a1b0
Philipp Bleimund <bleimund.philipp@gmail.com>
2024-06-09 17:43:30 +0200

    always compute fastest path

--- backtrack.py
+++ backtrack.py
@@ -47,15 +47,14 @@ def parse_history_to_rgb(paths_taken, field):
 
 def parse_paths_to_rgb(paths_taken, field):
     """parses the input field to an RGB tupel map"""
-    global maze
     rgb_field = np.zeros(shape=(len(field), len(field[0]), 3), dtype=int)
     for i in range(0, len(paths_taken)):
-        if field[paths_taken[i]] == MARKER:
-            rgb_field[paths_taken[i]] = rgb_field[paths_taken[i]] + 50
+        if field[paths_taken[i]] == MARKER or field[paths_taken[i]] == FREE:
+            rgb_field[paths_taken[i]] = rgb_field[paths_taken[i]] + 200
 
     for i in range(0, len(field)):
         for j in range(0, len(field[0])):
-            if field[i, j] != MARKER:
+            if field[i, j] != MARKER and field[i, j] != FREE:
                 rgb_field[i, j] = COLOR_MAP[field[i, j]]
     return rgb_field
 
@@ -79,13 +78,13 @@ def printArr(arr):
         print()
 
 
-def is_free(row, column, already_taken):
+def is_free(row, column, path):
     """return if the cell(row, column) is free"""
     if row < 0 or column < 0 or row >= len(maze) or column >= len(maze[row]):
         return False
     wall = maze[row, column] == WALL
-    marker = already_taken[row, column] == MARKER
-    return not (wall or marker)
+    already_taken = (row, column) in path
+    return not (wall or already_taken)
 
 
 def is_escape(row, column):
@@ -122,59 +121,77 @@ def is_dead_end(row, column):
     return get_free_sides(row, column) == 1
 
 
-# maze = maze_gen.getMaze(10, 10, 50)
+#maze = maze_gen.getMaze(10, 10, 15)
 convert_file_to_field("field3.txt")
 print(maze)
 printArr(maze)
 paths = []
 all_paths = []
+num_solves = 0
 
 plt.ion()
 fig, ax = plt.subplots()
+text = ax.text(.01, .99, str(num_solves), color="white")
 image = ax.imshow(parse_field_to_rgb(maze))
 plt.show()
 
 
-def print_to_display():
-    data = parse_paths_to_rgb(paths, maze)
+def print_to_display(path, num):
+    data = parse_paths_to_rgb(path, maze)
     image.set_data(data)
+    text.set_text(str(num))
     fig.canvas.draw()
     fig.canvas.flush_events()
     # time.sleep(0.5)
 
-# for future change already_taken to singel list of tupel to save RAM
-def solve_maze(row, column, already_taken):
-    directions = []
-    directions.append((row + 1, column))
-    directions.append((row, column + 1))
-    directions.append((row - 1, column))
-    directions.append((row, column - 1))
 
-    global paths
+# for future change already_taken to singel list of tupel to save RAM
+def solve_maze(row, column, path):
+    directions = [
+        (row + 1, column),  # Down
+        (row, column + 1),  # Right
+        (row - 1, column),  # Up
+        (row, column - 1)   # Left
+    ]
+
     global all_paths
-    paths.append((row, column))
-    print_to_display()
+    global num_solves
+    path.append((row, column))
+    #print_to_display(path)
 
     if is_escape(row, column):
         print("solved")
-        all_paths.append(list(paths))
-        paths.pop()
-        return True
-
-    maze[row, column] = MARKER
+        #print(path)
+        #time.sleep(0.5)
+        if(num_solves % 1000) == 0:
+            print_to_display(path, num_solves)
+        num_solves += 1
+        #all_paths.append(list(path))
+        path.pop()
+        return
 
     for d in directions:
-        if is_free(d[0], d[1], already_taken):
-            new_taken = np.copy(already_taken)
-            solve_maze(d[0], d[1], new_taken)
+        if is_free(d[0], d[1], path):
+            solve_maze(d[0], d[1], path)
 
-    paths.pop()
+    path.pop()
 
 
 # convert_file_to_field(
 # "/home/philippbleimund/git/code-experimentation/aud_seminar/Seminar7/field2.txt"
 # )
 printArr(maze)
-solve_maze(1, 1)
+solve_maze(1, 1, [])
+
+best = all_paths[0]
+for a in all_paths:
+    if len(a) < len(best):
+        best = a
+
+print(best)
+
+plt.imshow(parse_paths_to_rgb(best, maze))
+plt.show()
+
 
 printArr(maze)
