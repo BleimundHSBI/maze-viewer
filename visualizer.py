8694faadcd0b4065202397575a8dd881005e3699
Philipp Bleimund <bleimund.philipp@gmail.com>
2025-02-23 21:14:40 +0100

    added breath search

--- visualizer.py
+++ visualizer.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from dataclasses import dataclass, field
+from queue import Queue
 
 import shortuuid
 from cmap import Colormap
@@ -166,6 +167,130 @@ class Backtrace(Algorithm):
         return colored_maze
 
 
+class Breadth(Algorithm):
+
+    COLORS = {
+        Algorithm.EMPTY: np.array([255, 255, 255, 255], dtype=np.uint8),
+        Algorithm.WALL: np.array([0, 0, 0, 255], dtype=np.uint8),
+        Algorithm.ESCAPE: np.array([0, 255, 0, 255], dtype=np.uint8),
+        "PATH": np.array([255, 0, 180, 255], dtype=np.uint8)
+    }
+
+    CMAP = Colormap([(0, "yellow"), ("red")])
+
+    def __init__(self, maze, start_pos, token_mapping):
+        super().__init__(maze, start_pos, token_mapping)
+
+        self.found = False
+        self.solved = False
+
+        self.queue = Queue()
+        self.queue.put(start_pos)
+
+        self.visited = set()
+        self.best_path = []
+        self.heat_map = np.zeros_like(self.maze, dtype=float)
+        self.current_heat = 0
+
+    def _normalize_maze(self, maze, token_mapping):
+        normalized_maze = np.zeros_like(maze, dtype=int)
+        for key, value in token_mapping.items():
+            if key == "EMPTY":
+                normalized_maze[maze == value] = self.EMPTY
+            elif key == "WALL":
+                normalized_maze[maze == value] = self.WALL
+            elif key == "ESCAPE":
+                normalized_maze[maze == value] = self.ESCAPE
+
+        return normalized_maze
+
+    def nextStep(self):
+        if self.solved == True:
+            return
+
+        if self.found == False:
+            self.breadth()
+        elif self.found == True:
+            self.getPath()
+
+    def breadth(self):
+
+        current_pos = self.queue.get()
+        self.visited.add(current_pos)
+        self.current_heat += 1
+        self.heat_map[current_pos] = self.current_heat
+
+        if self.maze[current_pos] == self.ESCAPE:
+            self.best_path.append(current_pos)
+            self.found = True
+            return
+
+        directions = [
+            (1 + current_pos[0], 0 + current_pos[1]),  # Down
+            (0 + current_pos[0], 1 + current_pos[1]),  # Right
+            (-1 + current_pos[0], 0 + current_pos[1]),  # Up
+            (0 + current_pos[0], -1 + current_pos[1]),  # Left
+        ]
+
+        for d in directions:
+            if d[0] >= 0 and d[0] < len(self.maze) and d[1] >= 0 and d[1] < len(self.maze[0]) and \
+                    (self.maze[d] == self.EMPTY or self.maze[d] == self.ESCAPE) and d not in self.visited:
+                self.queue.put(d)
+
+    def getPath(self):
+
+        current_pos = self.best_path[-1]
+
+        if current_pos == self.start_pos:
+            self.solved = True
+            return
+
+        directions = [
+            (1 + current_pos[0], 0 + current_pos[1]),  # Down
+            (0 + current_pos[0], 1 + current_pos[1]),  # Right
+            (-1 + current_pos[0], 0 + current_pos[1]),  # Up
+            (0 + current_pos[0], -1 + current_pos[1]),  # Left
+        ]
+
+        valid_fields = []
+        for d in directions:
+            if d[0] >= 0 and d[0] < len(self.maze) and d[1] >= 0 and d[1] < len(self.maze[0]) and self.heat_map[d] > 0:
+                valid_fields.append(d)
+
+        valid_fields = np.array(valid_fields)
+        next_field = np.argmin(self.heat_map[valid_fields[:, 0], valid_fields[:, 1]])
+        self.best_path.append(tuple(valid_fields[next_field]))
+
+    def getCurentState(self) -> np.ndarray:
+        colored_maze = np.empty(shape=(len(self.maze), len(self.maze[0]), 4), dtype=np.uint8)
+
+        # base maze
+        colored_maze[self.maze == self.EMPTY] = self.COLORS[self.EMPTY]
+        colored_maze[self.maze == self.WALL] = self.COLORS[self.WALL]
+        colored_maze[self.maze == self.ESCAPE] = self.COLORS[self.ESCAPE]
+
+        return colored_maze
+
+    def getHistoricalView(self) -> np.ndarray:
+
+        colored_maze = np.empty(shape=(len(self.maze), len(self.maze[0]), 4), dtype=np.uint8)
+        colored_maze[self.maze == self.EMPTY] = self.COLORS[self.EMPTY]
+
+        # draw heatmap
+        colored_maze = self.CMAP(self.heat_map / self.heat_map.max())
+
+        # base maze
+        colored_maze[self.maze == self.WALL] = self.COLORS[self.WALL]
+        colored_maze[self.maze == self.ESCAPE] = self.COLORS[self.ESCAPE]
+
+        # current best path
+        if self.best_path:
+            rows, cols = zip(*self.best_path)
+            colored_maze[rows, cols] = self.COLORS["PATH"]
+
+        return colored_maze
+
+
 def convert_file_to_field(filename):
     """searches in the ./ directory for string:filename and parses it's content to an two dimensional numpy.array"""
     maze = []
@@ -185,12 +310,12 @@ if __name__ == "__main__":
         "WALL": "#",
         "ESCAPE": "E"
     }
-    back = Backtrace(maze, (1, 1), tokens)
+    breath = Breadth(maze, (1, 1), tokens)
 
-    while not back.solved:
-        back.nextStep()
+    while not breath.solved:
+        breath.nextStep()
 
-    image = back.getHistoricalView()
+    image = breath.getHistoricalView()
     fig, ax = plt.subplots()
     im = ax.imshow(image)
     plt.show()
